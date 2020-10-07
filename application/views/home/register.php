f15fa0fe0a18ec3c8cae9b42fdf180ac541893c7
TristanDCP <tristandacostapinto@gmail.com>
2020-10-07 12:05:21 +0200

    Mise en place du register

--- application/views/home/register.php
+++ application/views/home/register.php
@@ -1,16 +1,46 @@
-<form action="">
-    <div class="form-group">
-        <label for="userMail">Mail</label>
-        <input type="email" name="userMail" id="userMail" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="userPassword">Password</label>
-        <input type="password" name="userPassword" id="userPassword" class="form-control">
-    </div>
-    <div class="form-group">
-        <label for="userConfirm">Confirm password</label>
-        <input type="password" name="userConfirm" id="userConfirm" class="form-control">
-    </div>
-    <input type="submit" value="Register" class="btn btn-primary">
+<form action="" method="post">
+	<div class="form-group">
+		<label for="userFirstName">Prénom</label>
+		<input type="text" name="userFirstName" id="userFirstName" class="form-control">
+	</div>
+	<div class="form-group">
+		<label for="userLastName">Nom de famille</label>
+		<input type="text" name="userLastName" id="userLastName" class="form-control">
+	</div>
+	<div class="form-group">
+		<label for="userGender">Genre</label>
+		<select class="form-control" id="userGender">
+			<option>Masculin</option>
+			<option>Féminin</option>
+			<option>Non-binaire</option>
+			<option>Autre</option>
+		</select>
+	</div>
+	<div class="form-group">
+		<label for="userGender">Sexualité</label>
+		<select class="form-control" id="userSexuality">
+			<option>Hétérosexuel</option>
+			<option>Homosexuel</option>
+			<option>Bisexuel</option>
+			<option>Autre</option>
+		</select>
+	</div>
+	<div class="form-group">
+		<label for="userMail">Adresse email</label>
+		<input type="email" name="userMail" id="userMail" class="form-control">
+	</div>
+	<div class="form-group">
+		<label for="userPassword">Mot de passe</label>
+		<input type="password" name="userPassword" id="userPassword" class="form-control">
+	</div>
+	<div class="form-group">
+		<label for="userConfirm">Confirmer votre mot de passe</label>
+		<input type="password" name="userConfirm" id="userConfirm" class="form-control">
+	</div>
+	<div class="form-group">
+		<label for="userProfilePic">Photo de profil</label>
+		<input type="file" class="form-control-file" id="userProfilePic">
+	</div>
+	<input type="submit" value="S'inscrire" class="btn btn-primary" name="registerButton">
 </form>
-<p>Already registered ? <a href="<?= base_url()?>login">Login</a></p>
+<p>Déjà inscrit ? Connectez vous <a href="<?= base_url()?>login">Connexion</a></p>
