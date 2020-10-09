9372f09e23bc530cf1729f7596d438b65924cc4c
Paul Guillon <alentak27@gmail.com>
2020-10-09 18:29:33 +0200

    Version présentée :  message de bienvenue, icon

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -3,5 +3,6 @@
 if(isset($_SESSION['user']))
     header('location:'.base_url().'search/index');
 ?>
+<p>Bienvenue ! Connectez-vous pour accéder au site.</p>
 <a href="<?= base_url('home/login')?>"><button class="btn btn-primary bg-pink">Connexion</button></a>
 <a href="<?= base_url('home/register')?>"><button class="btn btn-primary bg-pink">Inscription</button></a>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,7 +4,8 @@ session_start();
 <html>
 
 <head>
-    <title>Page</title>
+    <title>Meetic</title>
+    <link rel="icon" href="<?= base_url('assets\img\logo.ico') ?>" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url('assets\css\style.css') ?>">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
