8beb4557d8645132bfd462277d351dec25113d1d
TristanDCP <tristandacostapinto@gmail.com>
2020-10-09 11:54:07 +0200

    Debut css

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,14 +4,15 @@ session_start();
 <html>
 
 <head>
-    <title>CodeIgniter Tutorial</title>
+    <title>Page</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="..\assets\css\style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-pink">
         <a class="navbar-brand" href="<?= base_url() ?>"><img src="<?= base_url('assets/img/logo.png') ?>" width="50" alt="logo"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
