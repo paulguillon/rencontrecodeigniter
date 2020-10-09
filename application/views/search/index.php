3c8672dfcfbbe6eff833cb66b7403a2b65222c54
TristanDCP <tristandacostapinto@gmail.com>
2020-10-09 09:34:21 +0200

    remove var dump

--- application/views/search/index.php
+++ application/views/search/index.php
@@ -1,9 +1,3 @@
-<?php
-var_dump($_SESSION);
-//Redirect user if not logged in
-// if (!isset($_SESSION['user']))
-//     header('location:' . base_url('home/login'));
-?>
 <div class="flex-row">
     <?php for ($i = 0; $i < count($users); $i++) : ?>
         <div class="card flex-row flex-wrap">
